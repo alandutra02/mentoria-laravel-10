5199341fbc7d8ce89d9c896185e656761453fcf6
root <root@DESKTOP-OL2MSK9.>
2024-08-01 04:48:58 -0400

    paginacao

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,15 +2,30 @@
 namespace App\Http\Controllers;
 use App\Models\Produto;
 
+
 use Illuminate\Http\Request;
+
+
 
 
 class ProdutosController extends Controller
 {
-    public function index ()
-    {
-        $findProduto = Produto::all();
+    protected $produto;
 
+
+    public function __construct(Produto $produto)
+    {
+        $this->produto = $produto;
+    }
+    public function index (Request $request)
+    {
+        $pesquisar = $request->pesquisar;
+        // abaixo está um ORM Eloquent para buscar dados
+        $findProduto = $this->produto->getProdutosPesquisarIndex($pesquisar ? $pesquisar : '');
+        /* também pode ser usando da forma abaixo ao inves da forma acima:
+        $findProduto = $this->produto->getProdutosPesquisarIndex(search : $pesquisar ?? '');
+        */
+        //dd($findProduto);
         return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
